0661a8f63abe0b92fdc74edadba2a1d6fb73fa02
DevilAbbath <79130973+DevilAbbath@users.noreply.github.com>
2024-07-03 02:58:41 -0400

    carrusel y nav ok

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+   console.log("Script loaded");
+   $('div.carouselindicators button.custom-btn1').addClass('active-page');
+
+   $('#carouselExampleIndicators').on('slid.bs.carousel', function () {
+      console.log("Slide Changed");
+      var slideUbication = $('div.carousel-inner div.carousel-item.active').index();
+
+      if (slideUbication === 0) {
+         console.log('Page ' + slideUbication + ' Show')
+         $('div.carouselindicators button.custom-btn1').addClass('active-page');
+         $('div.carouselindicators button.custom-btn2').removeClass('active-page');
+      } else if (slideUbication === 1) {
+         console.log('Page ' + slideUbication + ' Show')
+         $('div.carouselindicators button.custom-btn1').removeClass('active-page');
+         $('div.carouselindicators button.custom-btn2').addClass('active-page');
+      }
+   })
+});
